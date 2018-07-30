d3cc29baef7d3ecb1e698871f4af40e4852fc793
elizabeth <ekeleshian@gmail.com>
2018-07-29 17:57:55 -0700

    fetch functions now return date values as well as btc prices

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 import json
-from matplotlib import pyplot
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
 api_endpoint ='https://api.coindesk.com/v1/bpi/historical/close.json'
 
 def fetch_us_btc_data():
@@ -8,11 +10,14 @@ def fetch_us_btc_data():
 	json_file_btc = r.json()
 	btc_price_dict = json_file_btc['bpi']
 	btc_prices = []
+	btc_dates = []
 
 	for i in btc_price_dict:
 		btc_prices.append(btc_price_dict[i])
+		btc_dates.append(i)
 
-	return btc_prices
+
+	return btc_dates, btc_prices
 
 def fetch_uk_btc_data():
 	r = requests.get(api_endpoint+'?currency=GBP&start=2018-04-01&end=2018-07-01')
@@ -25,3 +30,6 @@ def fetch_uk_btc_data():
 
 	return btc_prices
 
+
+dates, prices = fetch_us_btc_data()
+print(dates)
