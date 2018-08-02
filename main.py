82ebcdd7c15890db2bd958ba8a54514e392a5809
elizabeth <ekeleshian@gmail.com>
2018-08-01 20:57:21 -0700

    added all components for linear regression model

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import json
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
+
+
 api_endpoint ='https://api.coindesk.com/v1/bpi/historical/close.json'
 
 def fetch_us_btc_data():
@@ -37,7 +39,49 @@ us_dates, us_prices = fetch_us_btc_data()
 
 uk_dates, uk_prices = fetch_uk_btc_data()
 
-plt.plot_date(x = us_dates, y=us_prices, color = 'b')
-plt.plot_date(x=uk_dates, y=uk_prices, color = 'r')
-plt.legend(['US = Blue, UK = Red'])
-plt.show()
+# plt.plot_date(x = us_dates, y=us_prices, color = 'b')
+# plt.plot_date(x=uk_dates, y=uk_prices, color = 'r')
+# plt.legend(['US = Blue, UK = Red'])
+# plt.show()
+
+x = us_prices
+y = uk_prices
+
+n = len(us_prices)
+
+def sum_of_products(x,y):
+	array_of_products = []
+	for a, b in zip(x,y):
+		array_of_products.append(a*b)
+	return sum(array_of_products)
+
+
+def sum_of_variables(x):
+	sigma = 0
+	for i in x:
+		sigma += i
+	return sigma
+
+
+def sum_of_squares(x):
+	array_of_squares = []
+	for i in x:
+		array_of_squares.apend(i*i)
+	return array_of_squares
+
+
+def estimated_slope(x, y):
+	numerator = n * sum_of_products(x, y) - sum_of_squares(x)*sum_of_squares(y)
+	denominator = n * sum_of_squares(x) - sum_of_squares(x)*sum_of_squares(x)
+
+	return float(numerator / denominator)
+
+
+def estimated_y0(x,y):
+	m = estimated_slope(x, y)
+	first_term = float(1/n)*sum_of_variables(y)
+	second_term = float(m/n)*sum_of_variables(x)
+	return first_term - second_term
+
+# print(min(x), max(x))
+# print(min(y), max(y))
